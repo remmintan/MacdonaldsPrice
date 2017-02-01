d19b0b28d5660e2d9067c065c934244e2fc21a20
remmintan <lwjgl@mail.ru>
2017-02-02 00:39:01 +0300

    Started experiments with algorims. Need some time to figure out the best way to make the best offer

--- burgerguru/macprice/urls.py
+++ burgerguru/macprice/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
+from . import views
+
 urlpatterns = [
-	
+	url(r'^(?P<sum>\w+)/$', views.DevView.as_view()),
 ]
--- burgerguru/macprice/views.py
+++ burgerguru/macprice/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class DevView(View):
+	def get(self, request, sum):
+		return render(request, 'macprice/index.html', { 'sum':sum })
+
