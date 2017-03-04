e24deabad1e0378c4e709a46b2e2b0bf9b84e1cb
remmintan <lwjgl@mail.ru>
2017-03-04 18:34:16 +0300

    Критический баг был обнаружен! Срочный фикс!

--- guru/burgerguru/wsgi.py
+++ guru/burgerguru/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "guru.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "burgerguru.settings")
 
 application = get_wsgi_application()
