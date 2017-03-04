71cad4380536dd784ab05441a7d26bac132c63a2
remmintan <lwjgl@mail.ru>
2017-03-04 17:08:22 +0300

    Отказ от испльзования MySQL в силу сложности миграции Unicode символов. Остаемся на PostgreSQL на неопределенное время.

--- guru/macprice/models.py
+++ guru/macprice/models.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-
+from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
@@ -10,18 +10,17 @@ class Resturant(models.Model):
     short_name = models.CharField(max_length=3)
     long_name = models.CharField(max_length=30)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.long_name
+
 
 class ProductGroup(models.Model):
     group_name = models.CharField(max_length=50)
     priority = models.IntegerField(default=10)  # default rating is really low
     resturant = models.ForeignKey(Resturant)
 
-    def __str__(self):
+    def __unicode__(self):
         return self.group_name
-
-
 
 
 class Product(models.Model):
@@ -45,11 +44,12 @@ class Product(models.Model):
         self.ccal = ccals
         self.save()
 
-    def __str__(self):
+    def __unicode__(self):
         if self.product_type == "N":
             return "{0} - {1} ({2})".format(self.product_name, self.price, self.ccal)
         else:
             return "{0} {2} - {1} ({3})".format(self.product_name, self.price, self.product_type, self.ccal)
+
 
 class User(models.Model):
     name = models.CharField(max_length=50)
@@ -70,7 +70,7 @@ class User(models.Model):
             self.surname = surname
             self.save()
 
-    def __str__(self):
+    def __unicode__(self):
         return "%s %s - %i" % (self.name, self.surname, self.pk)
 
 
@@ -79,7 +79,7 @@ class Chat(models.Model):
     requests = models.IntegerField(default=0)
     lastRequest = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
+    def __unicode__(self):
         if self.chatType == "private":
             user = User.objects.filter(pk=self.pk)[0]
             return "%s %s - %i. Last request: %s.%s.%s %s:%s" % (
